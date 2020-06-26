d237d7d38cd1d7f1a34f919beca80418aa8c985e
Batya <subatyais@gmail.com>
2020-06-26 23:06:26 +1000

    average cubic weight specs and implemetation

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const cubicWeight = ({length, height, width}) => {
     const conversionFactor = 250;
     
@@ -5,10 +7,17 @@ const cubicWeight = ({length, height, width}) => {
     
     return parseFloat(cubicWeight.toFixed(2));
 };
-    
+
+const averageCubicWeight = cubicWeightsArray => (
+    cubicWeightsArray.reduce(reducer, 0) / cubicWeightsArray.length
+);
+     
 // private
 const dimentionInMeters = ({length, height, width}) => (length /100 * height / 100 * width / 100);
 
+const reducer = (total, value) => total + value;
+
 module.exports = {
-    cubicWeight
+    cubicWeight,
+    averageCubicWeight
 }
--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -6,5 +6,14 @@ describe("Cubic Weight", () => {
         const cubicWeight = utils.cubicWeight({length: 45, height: 20, width: 30});
 
         expect(cubicWeight).toEqual(6.75);        
-    });    
+    });  
+});  
+
+describe("Average Cubic Weight", () => {
+    test('should return an average of 6.00 when there are 3 parcels with [6.00, 5.00, 7.00] cubic dimentions', () => {
+        
+        const avgCubicWeight = utils.averageCubicWeight([6.00, 5.00, 7.00]);
+
+        expect(avgCubicWeight).toEqual(6.00);        
+    });  
 });
